528967161748c4468b5679ebc787618d2e91e3ea
FelipeTiberio <felipetiberio@yahoo.com.br>
2019-03-01 10:42:56 -0300

    Acertei o primeiro print

--- fistgraph.py
+++ fistgraph.py
@@ -49,12 +49,11 @@ class Graph(object):
 
 	    for edge in self.edges:
 	    	value = edge.value
-        	from_node_value = edge.node_from.value
-        	to_node_value = edge.node_to.value
-        	tupla = (value, from_node_value, to_node_value)
-        	edge_list.append(tupla)
-
-        return edge_list
+	    	from_node_value = edge.node_from.value
+	    	to_node_value = edge.node_to.value
+	    	tupla = (value, from_node_value, to_node_value)
+	    	edge_list.append(tupla)
+	    return edge_list
 
 '''
     def get_adjacency_list(self):
@@ -83,7 +82,7 @@ graph.insert_edge(101, 1, 3)
 graph.insert_edge(102, 1, 4)
 graph.insert_edge(103, 3, 4)
 # Should be [(100, 1, 2), (101, 1, 3), (102, 1, 4), (103, 3, 4)]
-#print graph.get_edge_list()
+print (graph.get_edge_list())
 # Should be [None, [(2, 100), (3, 101), (4, 102)], None, [(4, 103)], None]
 #print graph.get_adjacency_list()
 # Should be [[0, 0, 0, 0, 0], [0, 0, 100, 101, 102], [0, 0, 0, 0, 0], [0, 0, 0, 0, 103], [0, 0, 0, 0, 0]]
