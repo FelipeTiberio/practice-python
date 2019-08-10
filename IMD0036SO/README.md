e66745d4a1e8e545521753a40e69aae7df333514
FelipeTiberio <felipetiberio@yahoo.com.br>
2019-08-09 21:47:53 -0300

    add

--- IMD0036SO/README.md
+++ IMD0036SO/README.md
@@ -7,12 +7,16 @@
 	[ ] Encerrar um processo através de alguma opção na tela, passando o PID do processo que se deseja encerrar.
 
 	[ ] Os processos que vocês criaram devem dobrar a cada 10 segundos.
-	([] Todo processo deve ficar vivo por no máximo 30 segundos se não for morto por outro processo.)
+	([ ] Todo processo deve ficar vivo por no máximo 30 segundos se não for morto por outro processo.)
 
 	[ ] A tela de exibição deve ser sempre atualizada quando houver criação e no término de um novo processo.
 
 
 ## links úteis :
 	
-	comodo ps e suas flas:
+	Comodo ps e suas flas:
 	https://www.tecmint.com/ps-command-examples-for-linux-process-monitoring/
+
+	str.format():
+	https://www.digitalocean.com/community/tutorials/how-to-use-string-formatters-in-python-3
+
--- IMD0036SO/gerenproc.py
+++ IMD0036SO/gerenproc.py
@@ -18,33 +18,32 @@ def get_matrix_process( str_lines ):
 
 	return matrix
 
-def get_process( mtx, user = all , amount = -1, fullinfo = False):
+def get_process( mtx, user ='all' , amount = -1, fullinfo = True):
 	""" @arg user, imprime apenas os processo de user
 		@arg amout, imprime os amount's primeiros processos
 		@arg fullinfo, Quando False, retorna apenas uma quantidade minima de informações de cada processo 
 			já quando True impre todo o retorno do comando ps -flag
 	"""
+	is_first_line  = True
+	str_formated = []
+	i = 0
 
+	for line in mtx:
+		for i in range(len(line)):
+			if user == 'all' and fullinfo:
+				str_aux = "| {user:^5} {pid:^5} {men:^5} {comand:^5} |".format(user = line[0], pid=line[1], cpr= line[2], men= line[3], comand=line[10])
+		str_formated.append(str_aux)
 
+	return str_formated 
+			
+### debug 
 mtx = get_matrix_process( checkout_comand( teste2 ))
+#for i in range(10):
+#	print(mtx[i])
 
-
-for i in range(10):
-	print(mtx[i])
-
-
-	
-	
+for i in get_process(mtx):
+	print(i)
 			
-#for i in checkout_comand( teste2 ):
-#	print(i)
-			
-
-#txt = "welcome to the jungle"
-
-#x = txt.split( maxsplit=1)
-
-#print(x)
 
 
 	
