d338ebbf757403ae85c11f4b0a8e38fe35ec5104
FelipeTiberio <felipetiberio@yahoo.com.br>
2019-08-19 15:56:50 -0300

    Já deu por hoje

--- IMD0036SO/gen_process/gerenproc.py
+++ IMD0036SO/gen_process/gerenproc.py
@@ -1,17 +1,27 @@
-import  subprocess, os
-ENTRADACOMAND1= 'ps -aux | grep python3'
+import  subprocess, os, time,sys
+from threading import Thread
+ENTRADACOMAND1= 'ps -aux | grep python3 '
 
 
 def proc_start():
-	""" Inicializa um novo processo filho retornando o pid correspondente ao 
-	processo criado """
 	if os.getpid() != 0: # Zero significa que é a cópia
 		newpid = os.fork()
-		print( " ** Processo com pid", newpid, "criado. ")	
-	
+
 	if newpid == 0:
+		time.sleep(10)
+		Thread(target=proc_start).start()
+		time.sleep(20)
+		Thread(target=proc_start).start()
+		time.sleep(30)
+		Thread(target=proc_start).start()
 		os._exit(0)
-	return newpid
+		return
+
+	if os.getpid() != 0:
+			#print( " \n\t PROCESSO COM PID {pid:} CRIADO.\n".format(pid = newpid))
+			time.sleep(30)
+			os.waitpid(int( newpid ),0)	
+	return
 
 def checkout_comand( args  ):
 	""" retorna todos os processo do sisteam em uma lista, cada item da lista é uma linha do comando args   """
@@ -27,25 +37,25 @@ def get_matrix_process( str_lines ):
 		matrix.append( line.split( maxsplit= maxsplit ))
 	return matrix
 
-def get_process( mtx, user ='all' , amount = -1, fullinfo = True):
-	""" @arg user, imprime apenas os processo de user
-		@arg amout, imprime os amount's primeiros processos
-		@arg fullinfo, Quando False, retorna apenas uma quantidade minima de informações de cada processo 
-			já quando True impre todo o retorno do comando ps -flag
+def get_process_mtx_formated( mtx):
 	"""
-	#is_first_line  = True
+	Retorna a matrix formatada para imprimir na tela os processos e suas informaçoes 
+	"""
 	str_formated = []
-
 	for line in mtx:
 		for i in range(len(line)):
-			if user == 'all' and fullinfo:
-				str_aux = "| {user:12s} {pid:5s} {men:5s} {comand:56s} {pipe:5s}".format(user = line[0], pid=line[1], cpr= line[2], men= line[3], comand=line[10].split()[0],pipe="|" )
+			str_aux = "| {user:12s} {pid:5s} {men:5s} {comand:56s} {pipe:5s}".format(user = line[0], pid=line[1], cpr= line[2], men= line[3], comand=line[10].split()[0],pipe="|" )
 		str_formated.append(str_aux)
+	str_formated.append(" \t\t- Quantidade de processos: {ln:} processos ".format(ln = len(mtx)))
+	if len(mtx) > 1500:
+		print( " Uma quantidade muito grande de processos o programa será finalizado ")
+		sys.exit(0)
+
+	
 	return str_formated 
 
 def finalizarProcesso( pid ):
 	""" Dado o pid de um processo como input, tenta finalizar-lo """
-
 	if os.getpid() != 0:
 		try:  
 			os.waitpid(int(pid),0)
@@ -54,29 +64,31 @@ def finalizarProcesso( pid ):
 			print("   \t\t--- VALOR DE PID NÂO É VÀlIDO ----")
 			input()
 			return False
+
+	print("\n   \t\t--- PROCESSO COM PID {pidi:} FINALIZADO ----\n".format(pidi =pid ))
+	input()
+
 	return True
 
 def menu():
 	if os.getpid == 0:
 		os._exit(0)
-	print("** \t\t Menu \t\t **")
+	print("\n---------------- Menu ------------------\n")
 	print("1. Digite 1 para inicializar mais processos. ")
 	print("2. Digite 2 para finalizar algum processo ")
 	op = input()
 
 	if op == "1":
-		proc_start()
+		Thread(target=proc_start).start()
 		#@TODO Imprimir aqui a nova lista de processos com destaque aos novos processos 
 	if op == "2":
 		#@TODO opção para finalizar os processos 
 		print(" \n\t DIGITE O VALOR DO PID PARA SER FINALIZADO ")
 		op = input()
-		finalizarProcesso(op)
+		#finalizarProcesso(op)
+		Thread( target=finalizarProcesso, args=(op,))
 		return
-
-### debug 
-
-x = 0
+		
 
 
 while True:
@@ -87,11 +99,9 @@ while True:
 	if pid_corrente == 0:
 		os._exit(0)
 		continue
-	
-	print("pid corrente",pid_corrente, " ",  os.getpid())
 
 	if pid_corrente != 0:
-		for i in get_process(mtx):
+		for i in get_process_mtx_formated(mtx):
 			print(i)
 		menu()
 
