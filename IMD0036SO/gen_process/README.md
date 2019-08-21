78db1e022400b37d929f965e58f11c0726df9677
FelipeTiberio <felipetiberio@yahoo.com.br>
2019-08-21 17:30:54 -0300

    Terminei a atividade de so

--- IMD0036SO/gen_process/README.md
+++ IMD0036SO/gen_process/README.md
@@ -2,12 +2,12 @@
 
 	[x] presentando-os em uma tela, mostrando o PID e o usuário que o criou, no mínimo.
 
-	[ ] Apresentar uma opção na tela para se criar mais processos.
+	[X] Apresentar uma opção na tela para se criar mais processos.
 
 	[x] Encerrar um processo através de alguma opção na tela, passando o PID do processo que se deseja encerrar.
 
-	[ ] Os processos que vocês criaram devem dobrar a cada 10 segundos.
-	([ ] Todo processo deve ficar vivo por no máximo 30 segundos se não for morto por outro processo.)
+	[X] Os processos que vocês criaram devem dobrar a cada 10 segundos.
+	([X] Todo processo deve ficar vivo por no máximo 30 segundos se não for morto por outro processo.)
 
 	[ ] A tela de exibição deve ser sempre atualizada quando houver criação e no término de um novo processo.
 
--- IMD0036SO/gen_process/gerenproc.py
+++ IMD0036SO/gen_process/gerenproc.py
@@ -1,34 +1,32 @@
 import  subprocess, os, time,sys, signal
 from threading import Thread
-ENTRADACOMAND1= 'ps -aux | grep python3 '
+ENTRADACOMAND1= 'ps -aux  '
 
 def proc_start():
+	"""Inicia um processo que irá criar outros 3 e morre apos 30 segundos """
 	if os.getpid() != 0:
 		newpid = os.fork()
 	
 	if newpid == 0:
+		#print(".. PROCESSO DE PID {proc:} CRIADO ".format(proc = os.getpid()))
 		time.sleep(10)
-		print(" PASSOU 10 SEGUNDOS {d:}".format(d = os.getpid()))
+		#print(" PASSOU 10 SEGUNDOS {d:}".format(d = os.getpid()))
 		Thread(target=proc_start).start()
 		time.sleep(10)
-		print(" PASSOU 20 SEGUNDOS {d:}".format(d = os.getpid()))
+		#print(" PASSOU 20 SEGUNDOS {d:}".format(d = os.getpid()))
 		Thread(target=proc_start).start()
 		time.sleep(10)
-		print(" PASSOU 30 SEGUNDOS {d:}".format(d = os.getpid()))
+		#print(" PASSOU 30 SEGUNDOS {d:}".format(d = os.getpid()))
 		Thread(target=proc_start).start()
 		print( ".. OS 30 SEGUNDOS DO {proc:} TERMINARAM, ELE IRÀ SER FINALIZADO ..".format( proc = os.getpid()))
 		finalizarProcesso(  int(os.getpid()) )
 		os._exit(0)
 
-	return
-
-def checkout_comand( args  ):
-	""" retorna todos os processo do sisteam em uma lista, cada item da lista é uma linha do comando args   """
+def get_matrix_process( args ):
+	"""Retorna uma matriz com a saida do comando ps -aux """
 	stdout = subprocess.check_output( args , shell=True)
-	return stdout.decode().split('\n')
+	str_lines = stdout.decode().split('\n')
 
-def get_matrix_process( str_lines ):
-	""" retorna uma matriz com o texo recebido """
 	matrix = []
 	maxsplit = (len(str_lines[0].split()))-1 # a quantidade de colunas de matrix sera os rotulos do output de subprocess(ps)
 	
@@ -37,22 +35,23 @@ def get_matrix_process( str_lines ):
 	return matrix
 
 def get_process_mtx_formated( mtx ):
-	"""
-	Retorna a matrix formatada para imprimir na tela os processos e suas informaçoes 
-	"""
+	"""Retorna a matrix formatada para imprimir na tela os processos e suas informaçoes """
 	str_formated = []
+
 	for line in mtx:
 		for i in range(len(line)):
 			str_aux = "{user:12s} {pid:5s} {men:5s} {comand:56s} {pipe:5s}".format(user = line[0], pid=line[1], cpr= line[2], men= line[3], comand=line[10].split()[0],pipe=" .. " )
 		str_formated.append(str_aux)
-	str_formated.append(" \t\t- Quantidade de processos: {ln:} processos ".format(ln = len(mtx)))
-	if len(mtx) > 1500:
+	str_formated.append(" \n\t\t.. QUANTIDADE DE PROCESSOS : {ln:} ..\n".format(ln = len(mtx)))
+
+	if len(mtx) > 1000: # Finaliza o programa caso seja criado muitos processos 
 		print( " Uma quantidade muito grande de processos o programa será finalizado ")
-		subprocess.call("killall python3")
+		subprocess.call(["killall", "python3"])
 	return str_formated 
 
 def printProcess():
-	mtx = get_matrix_process( checkout_comand( ENTRADACOMAND1 ))
+	""" Imprime na tela todos os processos do sisteam """
+	mtx = get_matrix_process(  ENTRADACOMAND1 )
 	if pid_corrente != 0:
 		for linha in get_process_mtx_formated(mtx):
 			print(linha)
@@ -101,6 +100,7 @@ if __name__ == '__main__':
 		if pid_corrente == 0:
 			os._exit(0)
 			continue
+
 		if pid_corrente != 0:
 			printProcess()
 			menu()
