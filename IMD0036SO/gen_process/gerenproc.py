32b4217165c0c42973efe3d64a7b0739f30810b2
FelipeTiberio <felipetiberio@yahoo.com.br>
2019-08-21 16:55:56 -0300

    Terminei a atividade, falta apenas refatorar e mandar

--- IMD0036SO/gen_process/gerenproc.py
+++ IMD0036SO/gen_process/gerenproc.py
@@ -1,26 +1,25 @@
-import  subprocess, os, time,sys
+import  subprocess, os, time,sys, signal
 from threading import Thread
 ENTRADACOMAND1= 'ps -aux | grep python3 '
 
-
 def proc_start():
-	if os.getpid() != 0: # Zero significa que é a cópia
+	if os.getpid() != 0:
 		newpid = os.fork()
-
+	
 	if newpid == 0:
 		time.sleep(10)
+		print(" PASSOU 10 SEGUNDOS {d:}".format(d = os.getpid()))
 		Thread(target=proc_start).start()
-		time.sleep(20)
+		time.sleep(10)
+		print(" PASSOU 20 SEGUNDOS {d:}".format(d = os.getpid()))
 		Thread(target=proc_start).start()
-		time.sleep(30)
+		time.sleep(10)
+		print(" PASSOU 30 SEGUNDOS {d:}".format(d = os.getpid()))
 		Thread(target=proc_start).start()
+		print( ".. OS 30 SEGUNDOS DO {proc:} TERMINARAM, ELE IRÀ SER FINALIZADO ..".format( proc = os.getpid()))
+		finalizarProcesso(  int(os.getpid()) )
 		os._exit(0)
-		return
 
-	if os.getpid() != 0:
-			#print( " \n\t PROCESSO COM PID {pid:} CRIADO.\n".format(pid = newpid))
-			time.sleep(30)
-			os.waitpid(int( newpid ),0)	
 	return
 
 def checkout_comand( args  ):
@@ -37,27 +36,32 @@ def get_matrix_process( str_lines ):
 		matrix.append( line.split( maxsplit= maxsplit ))
 	return matrix
 
-def get_process_mtx_formated( mtx):
+def get_process_mtx_formated( mtx ):
 	"""
 	Retorna a matrix formatada para imprimir na tela os processos e suas informaçoes 
 	"""
 	str_formated = []
 	for line in mtx:
 		for i in range(len(line)):
-			str_aux = "| {user:12s} {pid:5s} {men:5s} {comand:56s} {pipe:5s}".format(user = line[0], pid=line[1], cpr= line[2], men= line[3], comand=line[10].split()[0],pipe="|" )
+			str_aux = "{user:12s} {pid:5s} {men:5s} {comand:56s} {pipe:5s}".format(user = line[0], pid=line[1], cpr= line[2], men= line[3], comand=line[10].split()[0],pipe=" .. " )
 		str_formated.append(str_aux)
 	str_formated.append(" \t\t- Quantidade de processos: {ln:} processos ".format(ln = len(mtx)))
 	if len(mtx) > 1500:
 		print( " Uma quantidade muito grande de processos o programa será finalizado ")
-		sys.exit(0)
-
-	
+		subprocess.call("killall python3")
 	return str_formated 
+
+def printProcess():
+	mtx = get_matrix_process( checkout_comand( ENTRADACOMAND1 ))
+	if pid_corrente != 0:
+		for linha in get_process_mtx_formated(mtx):
+			print(linha)
 
 def finalizarProcesso( pid ):
 	""" Dado o pid de um processo como input, tenta finalizar-lo """
 	if os.getpid() != 0:
-		try:  
+		try:
+			os.kill(int(pid), signal.SIGKILL)  
 			os.waitpid(int(pid),0)
 			subprocess.call(['sleep','1'])
 		except:
@@ -67,43 +71,39 @@ def finalizarProcesso( pid ):
 
 	print("\n   \t\t--- PROCESSO COM PID {pidi:} FINALIZADO ----\n".format(pidi =pid ))
 	input()
-
 	return True
 
 def menu():
-	if os.getpid == 0:
+	pid_corrente = os.getpid()
+	if pid_corrente == 0:
 		os._exit(0)
 	print("\n---------------- Menu ------------------\n")
-	print("1. Digite 1 para inicializar mais processos. ")
-	print("2. Digite 2 para finalizar algum processo ")
+	print("1. Digite 1 para inicializar mais processos ... ")
+	print("2. Digite 2 para finalizar algum processo ... ")
 	op = input()
 
 	if op == "1":
 		Thread(target=proc_start).start()
-		#@TODO Imprimir aqui a nova lista de processos com destaque aos novos processos 
+		return
+
 	if op == "2":
-		#@TODO opção para finalizar os processos 
 		print(" \n\t DIGITE O VALOR DO PID PARA SER FINALIZADO ")
 		op = input()
-		#finalizarProcesso(op)
-		Thread( target=finalizarProcesso, args=(op,))
-		return
-		
+		finalizarProcesso(op)
+	return
 
 
-while True:
-		
-	pid_corrente = os.getpid()
-	mtx = get_matrix_process( checkout_comand( ENTRADACOMAND1 ))
+if __name__ == '__main__':
 
-	if pid_corrente == 0:
-		os._exit(0)
-		continue
+	while True:
+		pid_corrente = os.getpid()
 
-	if pid_corrente != 0:
-		for i in get_process_mtx_formated(mtx):
-			print(i)
-		menu()
-
+		if pid_corrente == 0:
+			os._exit(0)
+			continue
+		if pid_corrente != 0:
+			printProcess()
+			menu()
+	
 	
 	
