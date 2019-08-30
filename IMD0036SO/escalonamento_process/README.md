3c101074971c3dd9e59369f7023649a0cdc50ff4
Felipe Tiberio <felipetiberio@yahoo.com.br>
2019-08-30 19:42:01 -0300

    acho que fiz 50%  da atividade03b escalonamento

--- IMD0036SO/escalonamento_process/README.md
+++ IMD0036SO/escalonamento_process/README.md
@@ -17,3 +17,21 @@
     esse número deverá ser o tempo que a thread vai executar, simbolizando o tempo que ela tem 
     dentro da CPU. Quando esse tempo acaba o algortimo de escalonamento deverá escolher uma outra
     thread para ser executada pela CPU, chamando a função CPU novamente e passando a thread escolhida como parâmetro.
+
+
+
+    ----------------------------
+       Múltiplas Filas[7][8]: São usadas várias filas de processos prontos para executar, cada processo é colocado em uma fila, e cada fila tem uma política de escalonamento própria e outra entre filas, preemptivo, cada fila tem um determinado nível de prioridade, sendo um dos mais antigos agendadores de prioridade, estava presente no CTSS (Compatible Time-Sharing System - Sistema Compatível de Divisão por Tempo).No algoritmo de Múltiplas Filas, também pode ser aplicado particularmente, em cada fila, diferentes algoritmos como por exemplo, o algoritmo RR ou FCFS.
+
+      Para que o processo em uma fila seja executado, todas as filas de prioridade mais altas do que deveriam estar vazias, o que significa que o processo nessas filas de alta prioridade deve ter concluído sua execução. Nesse algoritmo de agendamento, uma vez atribuído a uma fila, o processo não será movido para nenhuma outra fila.
+
+      ##Problemas
+
+
+      Starvation: Para evitar esse problema, o Sistema Operacional pode aplicar prioridades dinâmicas. Neste caso, os processos que não tiveram acesso à CPU durante muito tempo, têm sua prioridade momentaneamente elevada. Em sistemas interativos esse mesmo mecanismo também pode ser empregado, porque o usuário deve possuir uma prioridade alta para poder trocar de programa quando desejar.
+
+
+
+      https://pt.wikipedia.org/wiki/M%C3%BAltiplas_filas
+
+
--- IMD0036SO/escalonamento_process/porPrioridade/prioridade.py
+++ IMD0036SO/escalonamento_process/porPrioridade/prioridade.py
@@ -1,6 +1,6 @@
-from threading import Thread, Lock, Condition, enumerate
+from threading import Thread, Condition, enumerate
 from random import randint
-import time, os
+import time
 
 Quantum = 5 # Tempo que cada processo terá no processador para terminar sua tarefa antes de ir para o fim da fila   
 
@@ -10,7 +10,10 @@ MAX = 15
                 
 MAXTHREADS = 5 # O número maximo de threads eh entre 3 e MAXTHREADS
 
-filaDeThreads = [] 
+MAXVALORPRIORIDADE = 5 # IREI gerar os valores de prioridade de forma randomica de 0 a MAXVALORPRIORIDADE
+
+filasDePrioridade = {} # {valor_prioridade : fila com o valor_prioridade} 
+ 
 
 def cpu( thread_da_vez  ):
     """Executa uma thread ate que ela finalize seu processo \n
@@ -23,14 +26,16 @@ def cpu( thread_da_vez  ):
         thread_da_vez.continuar_processo()
 
 class MinhaThread( Thread ):
-    def __init__(self, id, tempo):
+    def __init__(self, id, tempo, prioridade = 0 ):
         super(MinhaThread, self).__init__()
-        self.id = id
-        self.can_woke = True
-        self.condition = Condition()
-        self.tempo = tempo
-        self.falta = tempo
-        self.terminei = False
+        self.id             = id
+        self.can_woke       = True
+        self.condition      = Condition()
+        self.tempo          = tempo
+        self.falta          = tempo
+        self.terminei       = False
+        self.prioridade     = prioridade
+        self.tempo_de_fila  = 0
     
     def run(self):
         with self.condition:
@@ -39,9 +44,10 @@ class MinhaThread( Thread ):
                     self.condition.wait()  
                 print("* Processando thread nº", self.id  , "falta", self.falta, " para acabar meus procesos xD.")
                 time.sleep(1)
-                self.falta = self.falta -1 
+                self.falta  = self.falta -1 
+
             print(" \t-Terminei de processar, sou a thread ", self.id, ".")
-            self.terminei = True
+            self.terminei   = True
 
     def esperar(self):
         self.can_woke =False
@@ -53,37 +59,38 @@ class MinhaThread( Thread ):
             self.condition.notify()
             self.can_woke = True
 
+    def aumentarPrioridade(self):
+        self.prioridade +=1
+
 
 def configuracao_inicial():
-    print("| Quantidade de Threads: ", len(filaDeThreads) , " Tempo de quantum de cpu :", Quantum, "segundos |\n")
+    quantidade_t = 0
+    for i in filasDePrioridade:
+        quantidade_t += len(filasDePrioridade[i])
+    print("| Quantidade de Threads: ", quantidade_t  , " Tempo de quantum de cpu :", Quantum, "segundos |\n") #TODO tenho de fazer um novo
+    #@TODO a quantidade de cada uma das filas
 
 def polular_fila():
     numero_de_threads = randint(3, MAXTHREADS)
     id_thread = 1
 
-    for i in range(0,numero_de_threads):
-        tempo_processamento = randint(MIN, MAX)
-        filaDeThreads.append( MinhaThread(id=id_thread, tempo=tempo_processamento))
-        id_thread +=1
-
-def round_robin():
+    for valor_prioridade in range(0, MAXVALORPRIORIDADE):   # Chaves para o dicionario
+        if valor_prioridade not in filasDePrioridade:
+            filasDePrioridade[valor_prioridade] = []
     
-    while len(filaDeThreads) > 0  :
+    for i in range(0,numero_de_threads):                    # Colocando cada thread na fila correta 
 
-         thread_da_vez = filaDeThreads.pop(0)
-         if thread_da_vez.terminei or thread_da_vez.falta == 0: # linha onde removo as threads que acabaram da fila
-             continue
-         cpu(thread_da_vez)
-         time.sleep(Quantum)
-         thread_da_vez.esperar()
-         filaDeThreads.append(thread_da_vez)
+        tempo_processamento = randint(MIN, MAX)
+        prioridade          = randint(0,MAXVALORPRIORIDADE-1)
+        nova_thread         = MinhaThread(id_thread, tempo_processamento, prioridade)
+        filasDePrioridade[prioridade].append(nova_thread)
+        id_thread           +=1
+
+
 
 
 if __name__ == '__main__':   
 
     polular_fila()
     configuracao_inicial()
-    round_robin()
-
-
-
+    print(filasDePrioridade)
