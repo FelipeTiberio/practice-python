c9437feb33efa3f81281eaab9416cd2533ba3eaf
Felipe Tiberio <felipetiberio@yahoo.com.br>
2019-08-30 17:27:46 -0300

    Terminei a atividade3 item 1

--- IMD0036SO/escalonamento_process/README.md
+++ IMD0036SO/escalonamento_process/README.md
@@ -1,17 +1,19 @@
- - [] (O primeiro algoritmo a ser implementado é o Round-Robin (ou circular),
+ - [x] (O primeiro algoritmo a ser implementado é o Round-Robin (ou circular),
        onde os processos são executados um atrás do outro e não uso de prioridades. 
        O primeiro processo depois de executado deverá retornar para o final da fila.)
- - [] (O segundo algoritmo a ser implementado é o Por Prioridades, onde existem diversas
+
+ - [ ] (O segundo algoritmo a ser implementado é o Por Prioridades, onde existem diversas
     classes de prioridades de processos , onde os processos dentro dessa classe são
     executados de forma circular. A  classe com prioridade mais alta deve ser executada 
     primeiro antes das outras, e, as outras só poderam ser executadas quando a
     classe de prioridade maior acabar com os seus processos, a não ser que aja algum controle para evitar starvation.)
 
- - [] (Deverão ser utilizadas pelo menos 3 threads para a realização da tarefa.)
+ - [x] (Deverão ser utilizadas pelo menos 3 threads para a realização da tarefa.)
 
- - [] (Os programas deverão conter além dos algoritmos de escalonamento uma função CPU onde deverá receber 
+ - [x] (Os programas deverão conter além dos algoritmos de escalonamento uma função CPU onde deverá receber 
        como parâmetro uma thread e iniciar a sua execução.)
- - [] Cada função que as threadas estarão executando receberão como parâmetro um número, 
+
+ - [ ] Cada função que as threadas estarão executando receberão como parâmetro um número, 
     esse número deverá ser o tempo que a thread vai executar, simbolizando o tempo que ela tem 
     dentro da CPU. Quando esse tempo acaba o algortimo de escalonamento deverá escolher uma outra
     thread para ser executada pela CPU, chamando a função CPU novamente e passando a thread escolhida como parâmetro.
--- IMD0036SO/escalonamento_process/round-robin/round_robin.py
+++ IMD0036SO/escalonamento_process/round-robin/round_robin.py
@@ -2,12 +2,19 @@ from threading import Thread, Lock, Condition, enumerate
 from random import randint
 import time, os
 
-Quantum = 5 
+Quantum = 5 # Tempo que cada processo terá no processador para terminar sua tarefa antes de ir para o fim da fila   
 
-filaDeThreads = []
+#'''Eu fiz com que cada um das thread tenham um tempo de processamento randomico entre MIN e MAX '''
+MIN = 5  
+MAX = 15
+                
+MAXTHREADS = 5 # O número maximo de threads eh entre 3 e MAXTHREADS
+
+filaDeThreads = [] 
+
 def cpu( thread_da_vez  ):
     """Executa uma thread ate que ela finalize seu processo \n
-       essa pode não ter sido iniciada ainda, ou esta esperando """
+       a thread pode não ter sido iniciada ainda, ou esta esperando """
 
     if not thread_da_vez.isAlive():
         thread_da_vez.start()
@@ -16,30 +23,30 @@ def cpu( thread_da_vez  ):
         thread_da_vez.continuar_processo()
 
 class MinhaThread( Thread ):
-    def __init__(self, simbolo, id, tempo):
+    def __init__(self, id, tempo):
         super(MinhaThread, self).__init__()
-        self.simblo = simbolo
         self.id = id
         self.can_woke = True
         self.condition = Condition()
         self.tempo = tempo
         self.falta = tempo
-        self.terminoi = False
+        self.terminei = False
     
     def run(self):
         with self.condition:
             for i in range(self.tempo):
                 if not self.can_woke:
                     self.condition.wait()  
-                print(" processando thread nº", self.id , " ", self.simblo , "falta", self.falta)
+                print("* Processando thread nº", self.id  , "falta", self.falta, " para acabar meus procesos xD.")
                 time.sleep(1)
                 self.falta = self.falta -1 
-            print("Salut, terminei de processar, sou a thread ", self.id)
-            self.terminoi = True
+            print(" \t-Terminei de processar, sou a thread ", self.id, ".")
+            self.terminei = True
 
     def esperar(self):
         self.can_woke =False
-        print("Sou a thread ", self.id , " vou para o fim da fila... ")
+        if self.falta > 0:
+            print("\t-Sou a thread ", self.id , " meu tempo de quantum acabou, irei para o fim da fila... ")
         
     def continuar_processo(self):
         with self.condition:
@@ -51,28 +58,30 @@ def round_robin():
     while len(filaDeThreads) > 0  :
 
          thread_da_vez = filaDeThreads.pop(0)
-         if thread_da_vez.terminoi :
+         if thread_da_vez.terminei or thread_da_vez.falta == 0: # linha onde removo as threads que acabaram da fila
              continue
-
          cpu(thread_da_vez)
          time.sleep(Quantum)
          thread_da_vez.esperar()
          filaDeThreads.append(thread_da_vez)
 
-# teste 
-filaDeThreads.append( MinhaThread("|||",1,10))
-filaDeThreads.append( MinhaThread("|||",2,8))
+def configuracao_inicial():
+    print("| Quantidade de Threads: ", len(filaDeThreads) , " Tempo de quantum de cpu :", Quantum, "segundos |\n")
 
-'''
-t = MinhaThread('|||', 1, 10)
-cpu(t)
-t.esperar()
-time.sleep(3)
-cpu(t)
-#t.continuar_processo()
-'''
+def polular_fila():
+    numero_de_threads = randint(3, MAXTHREADS)
+    id_thread = 1
 
-round_robin()
+    for i in range(0,numero_de_threads):
+        tempo_processamento = randint(MIN, MAX)
+        filaDeThreads.append( MinhaThread(id=id_thread, tempo=tempo_processamento))
+        id_thread +=1
+
+if __name__ == '__main__':   
+
+    polular_fila()
+    configuracao_inicial()
+    round_robin()
 
 
 
