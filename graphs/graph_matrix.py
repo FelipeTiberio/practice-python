cc536fc88cc0551df7b78af5baec00c65c639ec9
FelipeTiberio <felipetiberio@yahoo.com.br>
2019-10-25 21:14:19 -0300

    ja sei como corrigir as cores e os id dos vértices

--- graphs/graph_matrix.py
+++ graphs/graph_matrix.py
@@ -1,6 +1,7 @@
 class Vertice():
-    def __init__(self, rotulo):
-        self.rotulo = rotulo
+    def __init__(self, id, cor):
+        self.id = id
+        self.cor = cor
 
 
 
@@ -10,17 +11,18 @@ class Grafo:
         self.adjMatrix = []
         self.vertices = []
 
+        # Preenchendo a matrix de adjacencia 
         for i in range(numVertice):
             linha = []
             for j in range(numVertice):
                 linha.append(0)
             self.adjMatrix.append(linha)
-        
+
+        # Criando todos os vertices do grafo
+        id = 0
         for i in range(numVertice):
-            linha = []
-            for j in range(numVertice):
-                linha.append(Vertice(None))
-            self.vertices.append(linha)
+            self.vertices.append(id, None)
+            id += 1
 
     def showMatrix(self):
         for i in self.adjMatrix:
@@ -28,8 +30,8 @@ class Grafo:
                 print(j , end= " ")
             print(" ")
 
-    def addAresta(self, linha, coluna, rotulo):
-
+    def addAresta(self, id_vertice1, id_vertice2, rotulo):
+        #@TODO Corrijá o código da que para baixo 
         if self.adjMatrix[linha][coluna] ==  1:
             self.vertices[linha][coluna] = Vertice(rotulo)
             return False
