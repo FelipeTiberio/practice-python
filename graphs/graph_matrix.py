ccacda7d798ef53c195ac3767ea7addee1325907
FelipeTiberio <felipetiberio@yahoo.com.br>
2019-11-01 12:49:45 -0300

    Terminei o grafo base para o problema

--- graphs/graph_matrix.py
+++ graphs/graph_matrix.py
@@ -4,11 +4,10 @@ class Vertice():
         self.cor = cor
 
 
-
 class Grafo:
     def __init__(self, numVertice) :
-        self.numVertice = numVertice 
-        self.adjMatrix = []
+        self.__numVertice = numVertice 
+        self.__adjMatrix = []
         self.vertices = []
 
         # Preenchendo a matrix de adjacencia 
@@ -16,49 +15,71 @@ class Grafo:
             linha = []
             for j in range(numVertice):
                 linha.append(0)
-            self.adjMatrix.append(linha)
+            self.__adjMatrix.append(linha)
 
         # Criando todos os vertices do grafo
         id = 0
         for i in range(numVertice):
-            self.vertices.append(id, None)
+            self.vertices.append( Vertice(id, None))
             id += 1
 
+    def addAresta(self, id_vertice1, id_vertice2):
+        if ( id_vertice1 == id_vertice2  ):
+            print("A vérteces são iguais.")
+            return
+
+        if ( id_vertice1 < 0 or id_vertice2 < 0 or id_vertice1 >= self.__numVertice or id_vertice2 >= self.__numVertice):
+            print("Valores de vértices não existem.")
+            return
+
+        self.__adjMatrix[id_vertice1][id_vertice2] = 1
+        self.__adjMatrix[id_vertice2][id_vertice1] = 1
+        
+        return True
+
     def showMatrix(self):
-        for i in self.adjMatrix:
+        for i in self.__adjMatrix:
             for j in i:
-                print(j , end= " ")
+                print("{:2}".format(j) , end= " ")
             print(" ")
 
-    def addAresta(self, id_vertice1, id_vertice2, rotulo):
-        #@TODO Corrijá o código da que para baixo 
-        if self.adjMatrix[linha][coluna] ==  1:
-            self.vertices[linha][coluna] = Vertice(rotulo)
+    def removeAresta(self,  id_vertice1, id_vertice2):
+
+        if ( id_vertice1 == id_vertice2  ):
+            print("A vérteces são iguais.")
+            return
+
+        if ( id_vertice1 < 0 or id_vertice2 < 0 or id_vertice1 >= self.__numVertice or id_vertice2 >= self.__numVertice):
+            print("Valores de vértices não existem.")
+            return
+
+        if self.__adjMatrix[ id_vertice1][id_vertice2] == 0:
             return False
 
-        self.adjMatrix[linha][coluna] = 1
+        self.__adjMatrix[ id_vertice1][id_vertice2] = 0
+        self.__adjMatrix[id_vertice2][ id_vertice1] = 0
+
         return True
 
-    def removeAresta(self, linha, coluna):
+    def existeAresta(self, v1, v2):
+        try:
+            return True if self.__adjMatrix[v1][v2] > 0 else False
 
-        if self.adjMatrix[linha][coluna] == 0:
-            return False
-        self.adjMatrix[linha][coluna] = 0
-        self.vertices[linha][coluna] = None
-        return True
-
-    def existeAresta(self, linha, coluna):
-        if self.adjMatrix[linha][coluna] > 0:
-            return True
-        else:
-            return False
+        except IndexError:
+            print("Valores:", v1,"e", v2, "não são íncides válidos" )
 
     def grauVertice(self, vertice):
         pass
 
+    def getMatrixAdj(self):
+        return self.__adjMatrix
+
+    def __len__(self):
+        return self.__numVertice
+
+
 g = Grafo(4)
 
-
-#g.addAresta(0, 1)
+g.addAresta(0, 1)
 
 g.showMatrix()
