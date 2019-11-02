276e5e3ceea5f27abdc0b53f818a3af326684b16
FelipeTiberio <felipetiberio@yahoo.com.br>
2019-11-02 17:41:55 -0300

    Update graph_matrix.py
    Add do código para as adjacencis de colunas e linhas

--- graphs/graph_matrix.py
+++ graphs/graph_matrix.py
@@ -1,38 +1,45 @@
 import math
 
 class Vertice():
-    def __init__(self, id, cor):
-        self.id = id
+    def __init__(self, tupla, cor = None):
+        """Atributo tupla (i,j) equivale a linha i e coluna j do jogo de Sudoku .\n
+        Atribito cor é o inteiro que marca o número preenchido no jogo."""
+        self.tupla = tupla
         self.cor = cor
+       
+    def __eq__(self, x):
+        if self.tupla == x.tupla:
+            return True
+        else:
+            return False
 
 class Grafo:
     def __init__(self, tamanho_bloco) :
         """ O argumento tamanho do bloco representa o tamanho do lado das regiões do sudoku.
             Para criar uma instancia de um sudoko do tipo 9x9 o tamanho do bloco será de 3. """
+
         self.__numVertice = pow(tamanho_bloco,2) * pow(tamanho_bloco,2)
         self.__adjMatrix = []
         self.vertices = []
-
-        # Preenchendo a matrix de adjacencia 
-        for i in range(self.__numVertice):
+        self.__n = int(math.sqrt(self.__numVertice))
+        
+        for i in range(self.__numVertice): # Preenchendo a matrix de adjacencia 
             linha = []
             for j in range(self.__numVertice):
                 linha.append(0)
             self.__adjMatrix.append(linha)
+       
+        for i in range( 0 , self.__n ): # Criando todos os vertices do grafo, estou usando tuplas (i,j) para representar a linha e coluna de cada vértice
+            for j in range(0 , self.__n ):
+                self.vertices.append( Vertice( ( int(i % self.__n) ,j),None ) )
 
-        # Criando todos os vertices do grafo
-        id = 0
-        for i in range(self.__numVertice):
-            self.vertices.append( Vertice(id, None))
-            id += 1
+
 
     def addAresta(self, id_vertice1, id_vertice2):
         if ( id_vertice1 == id_vertice2  ):
-            print("A vérteces são iguais.")
             return False
 
         if ( id_vertice1 < 0 or id_vertice2 < 0 or id_vertice1 >= self.__numVertice or id_vertice2 >= self.__numVertice):
-            print("Valores de vértices não existem.")
             return False
 
         self.__adjMatrix[id_vertice1][id_vertice2] = 1
@@ -99,31 +106,41 @@ class Grafo:
         return self.__numVertice
 
     def adjSudoku(self):
-        linha = 1
-        coluna = 1
+        """ Dois vértices (i,j) e (i´,j´) são adjacentes quando i=i´ ou j=j´ """
+        
+        for v1 in self.vertices:
+            for linha in range(self.__n): # adjacencia em colunas
+                v2 = Vertice( (linha, v1.tupla[1]), None ) 
 
-        for linha in self.__adjMatrix:
+                incide1 = self.vertices.index(v1)
+                incide2 = self.vertices.index(v2)
+                self.addAresta(incide2,incide1)
 
-            for celula in linha:
-            
-    
-    
-           
+            for coluna in range(self.__n): # adjacencia em linhas
+                v2 = Vertice( (v1.tupla[0],coluna ), None ) 
+                
+                incide1 = self.vertices.index(v1)
+                incide2 = self.vertices.index(v2)
+                self.addAresta(incide2,incide1)
 
-
+            for v2 in self.vertices:
+                if (math.ceil(v1.tupla[0]/self.__n) == math.ceil(v2.tupla[0]/self.__n) and math.ceil(v1.tupla[1]/self.__n) == math.ceil(v2.tupla[1]/self.__n) ):
+                    incide1 = self.vertices.index(v1)
+                    incide2 = self.vertices.index(v2)
+                    self.addAresta(incide2,incide1)
 
 
 def main():    
 
     g = Grafo(2)
-
-    g.addAresta(0, 1)
-    g.removeAresta(1,0)
-
+    
     g.adjSudoku()
+    
 
-    #g.showMatrix()
+#print("tamanho de g:", len(g))
+    g.showMatrix()
 
+   # print("floor(i/n)=floolr(i'/n)={i:} and floor(j/n)=floolr(j'/n) = {j:}".format(i=math.floor(2/2) ,j=math.floor(3/2)))
 
 
 if (__name__ == '__main__'):
